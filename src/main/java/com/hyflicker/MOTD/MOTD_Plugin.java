7a755c5827808c41980f39242b910b54eae5d327
Alyx Dev <hyflicker@gmail.com>
2026-01-24 12:36:43 -0500

    Init

---
+++ src/main/java/com/hyflicker/MOTD/MOTD_Plugin.java
@@ -0,0 +1,21 @@
+package com.hyflicker.MOTD;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+public class MOTD_Plugin extends JavaPlugin {
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public MOTD_Plugin(JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("MOTD Plugin %s initialized.", this.getManifest().getVersion());
+    }
+
+    @Override
+    protected void setup() {
+        ModConfig.get();
+        this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, OnPlayerJoin::onPlayerReady);
+    }
+}
