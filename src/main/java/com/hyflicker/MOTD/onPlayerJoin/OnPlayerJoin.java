98f2e2cbd6772189e0fdb00e58f89d325517bf83
Alyx Dev <hyflicker@gmail.com>
2026-01-27 18:48:04 -0500

    Reworked and renamed the on player join banner. Moved to new class.

---
+++ src/main/java/com/hyflicker/MOTD/onPlayerJoin/OnPlayerJoin.java
@@ -0,0 +1,45 @@
+package com.hyflicker.MOTD.onPlayerJoin;
+
+import com.hyflicker.MOTD.Configuration.ModConfig;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.util.EventTitleUtil;
+
+import java.awt.*;
+
+public class OnPlayerJoin{
+    public static void welcomeBanner(PlayerReadyEvent event){
+
+        ModConfig.WelcomeBanner bannerConfig = ModConfig.get().welcomeBanner;
+        if (bannerConfig.enabled) {
+            Player player = event.getPlayer();
+            PlayerRef playerRef = event.getPlayerRef().getStore().getComponent(event.getPlayerRef(),PlayerRef.getComponentType());
+
+            sendMotdTitle(playerRef, bannerConfig, player);
+        }
+    }
+
+    public static Message formatMessage(String template, Player player) {
+        if (template == null || template.isEmpty()) {
+            return Message.raw("");
+        }
+
+        String processed = template.replace("%player%", player.getDisplayName());
+        return Message.raw(processed);
+    }
+
+    public static void sendMotdTitle(PlayerRef playerRef, ModConfig.WelcomeBanner bannerConfig, Player player) {
+        EventTitleUtil.showEventTitleToPlayer(
+                playerRef,
+                formatMessage(bannerConfig.getPrimaryTitle(), player),
+                formatMessage(bannerConfig.getSecondaryTitle(), player),
+                bannerConfig.isMajor,
+                (String)null,
+                bannerConfig.duration,
+                bannerConfig.fadeInDuration,
+                bannerConfig.fadeOutDuration
+        );
+    }
+}
