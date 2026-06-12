b1c225a9d1702e49e77596945766c3781a77029b
Alyx Dev <hyflicker@gmail.com>
2026-06-12 01:43:27 -0400

    Reworked how we get the username. And passed the username into the title functions.

--- src/main/java/com/hyflicker/MOTD/onPlayerJoin/OnPlayerJoin.java
+++ src/main/java/com/hyflicker/MOTD/onPlayerJoin/OnPlayerJoin.java
@@ -1,47 +1,46 @@
 package com.hyflicker.MOTD.onPlayerJoin;
 
 import com.hyflicker.MOTD.Configuration.ModConfig;
+import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
-import com.hypixel.hytale.server.core.universe.Universe;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.core.util.EventTitleUtil;
-
-import java.awt.*;
 
 public class OnPlayerJoin{
     public static void welcomeBanner(PlayerReadyEvent event){
         ModConfig.WelcomeBanner bannerConfig = ModConfig.get().welcomeBanner;
         Player player = event.getPlayer();
-        String playerName = player.getDisplayName();
-        PlayerRef playerRef = Universe.get().getPlayers().stream()
-                .filter(p -> p.getUsername().equalsIgnoreCase(playerName))
-                .findFirst()
-                .orElse(null);
+        Ref<EntityStore> entityRef = event.getPlayerRef();
+        PlayerRef playerRef = entityRef.getStore().getComponent(entityRef, PlayerRef.getComponentType());
+
+        if (playerRef == null) return;
+        String playerName = playerRef.getUsername();
         if (bannerConfig.enabled || bannerConfig.firstJoin.enabled) {
-            sendMotdTitle(playerRef, bannerConfig, player);
+            sendMotdTitle(playerRef, bannerConfig, player, playerName);
         }
         if(player.isFirstSpawn() && bannerConfig.firstJoin.serverAnnounce.enabled){
-            sendMotdTitleUniverse(bannerConfig,player);
+            sendMotdTitleUniverse(bannerConfig, playerName);
         }
     }
 
-    public static Message formatMessage(String template, Player player) {
+    public static Message formatMessage(String template, String playerName) {
         if (template == null || template.isEmpty()) {
             return Message.raw("");
         }
-        String processed = template.replace("%player%", player.getDisplayName());
+        String processed = template.replace("%player%", playerName);
         return Message.raw(processed);
     }
 
-    public static void sendMotdTitle(PlayerRef playerRef, ModConfig.WelcomeBanner bannerConfig, Player player) {
+    public static void sendMotdTitle(PlayerRef playerRef, ModConfig.WelcomeBanner bannerConfig, Player player, String playerName) {
         //These get set by WelcomeBanner primary/secondary titles if randomized is turned on it returns randomized titles.
-        Message primaryTitle = formatMessage(bannerConfig.getPrimaryTitle(), player);
-        Message secondaryTitle = formatMessage(bannerConfig.getSecondaryTitle(), player);
+        Message primaryTitle = formatMessage(bannerConfig.getPrimaryTitle(), playerName);
+        Message secondaryTitle = formatMessage(bannerConfig.getSecondaryTitle(), playerName);
         if(player.isFirstSpawn() && bannerConfig.firstJoin.enabled){
-            primaryTitle = formatMessage(bannerConfig.firstJoin.getPrimaryTitle(bannerConfig.getPrimaryTitle()), player);
-            secondaryTitle = formatMessage(bannerConfig.firstJoin.getSecondaryTitle(bannerConfig.getSecondaryTitle()), player);
+            primaryTitle = formatMessage(bannerConfig.firstJoin.getPrimaryTitle(bannerConfig.getPrimaryTitle()), playerName);
+            secondaryTitle = formatMessage(bannerConfig.firstJoin.getSecondaryTitle(bannerConfig.getSecondaryTitle()), playerName);
         }
 
         EventTitleUtil.showEventTitleToPlayer(
@@ -56,10 +55,10 @@ public class OnPlayerJoin{
                 bannerConfig.fadeOutDuration
         );
     }
-    public static void sendMotdTitleUniverse(ModConfig.WelcomeBanner bannerConfig, Player player) {
+    public static void sendMotdTitleUniverse(ModConfig.WelcomeBanner bannerConfig, String playerName) {
         EventTitleUtil.showEventTitleToUniverse(
-                formatMessage(bannerConfig.firstJoin.serverAnnounce.getPrimaryTitle(), player),
-                formatMessage(bannerConfig.firstJoin.serverAnnounce.getSecondaryTitle(), player),
+                formatMessage(bannerConfig.firstJoin.serverAnnounce.getPrimaryTitle(), playerName),
+                formatMessage(bannerConfig.firstJoin.serverAnnounce.getSecondaryTitle(), playerName),
                 bannerConfig.firstJoin.serverAnnounce.isMajor,
 //                (String)null,
                 null,
