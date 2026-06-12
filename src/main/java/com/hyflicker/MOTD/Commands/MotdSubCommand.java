3e1eff3df9049dbf99cf99936c86524af7976f4d
Alyx Dev <hyflicker@gmail.com>
2026-06-12 10:16:38 -0400

    Moved all the permissions for all commands to this class and corrected permissions errors.

--- src/main/java/com/hyflicker/MOTD/Commands/MotdSubCommand.java
+++ src/main/java/com/hyflicker/MOTD/Commands/MotdSubCommand.java
@@ -1,6 +1,11 @@
 package com.hyflicker.MOTD.Commands;
 
+import com.hyflicker.MOTD.Configuration.ModConfig;
+import com.hypixel.hytale.server.core.command.system.CommandSender;
 import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+
+import javax.annotation.Nonnull;
 
 public abstract class MotdSubCommand extends CommandBase {
 
@@ -8,6 +13,23 @@ public abstract class MotdSubCommand extends CommandBase {
         super(name, description);
 
         // This keeps the server boot cycle happy across all sub-commands!
-        this.requirePermission("motd.use");
+        this.requirePermission(ModConfig.get().commandPermissions);
+    }
+
+    @Override
+    protected boolean canGeneratePermission(){
+        return false;
+    }
+
+    @Override
+    public boolean hasPermission(@Nonnull CommandSender sender){
+        String requiredPerm = ModConfig.get().commandPermissions;
+
+        if(sender instanceof PlayerRef playerRef){
+            return playerRef.hasPermission(requiredPerm)
+                    || playerRef.hasPermission("hytale.admin")
+                    || playerRef.hasPermission("hytale.command.op");
+        }
+        return true;
     }
 }
