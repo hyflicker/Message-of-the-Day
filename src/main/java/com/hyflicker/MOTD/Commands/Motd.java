3ab7f07fcc6f5b8cec02aa983dda090294585bdb
Alyx Dev <hyflicker@gmail.com>
2026-06-12 10:15:59 -0400

    Reworked permissions and moved it to the MotdSubCommand class

--- src/main/java/com/hyflicker/MOTD/Commands/Motd.java
+++ src/main/java/com/hyflicker/MOTD/Commands/Motd.java
@@ -2,20 +2,14 @@ package com.hyflicker.MOTD.Commands;
 
 import com.hyflicker.MOTD.Commands.Config.Config;
 import com.hyflicker.MOTD.Commands.Players.Player;
-import com.hyflicker.MOTD.Configuration.ModConfig;
-import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
-import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
-import com.hypixel.hytale.server.core.universe.PlayerRef;
 
 import javax.annotation.Nonnull;
-import java.awt.*;
 
-public class Motd extends CommandBase {
+public class Motd extends MotdSubCommand {
 
     public Motd(String pluginName) {
         super("motd", "Main command for " + pluginName);
-        this.requirePermission("motd.use");
         this.addSubCommand(new Reload());
         this.addSubCommand(new Test());
         this.addSubCommand(new Announcement());
@@ -25,19 +19,6 @@ public class Motd extends CommandBase {
 
     @Override
     protected void executeSync(@Nonnull CommandContext ctx) {
-        String requiredPerm = ModConfig.get().commandPermissions;
-
-        if (ctx.sender() instanceof PlayerRef playerRef) {
-            boolean hasAccess = playerRef.hasPermission(requiredPerm)
-                    || playerRef.hasPermission("hytale.admin")
-                    || playerRef.hasPermission("hytale.command.op");
-
-            if (!hasAccess) {
-                ctx.sendMessage(Message.raw("You do not have permission to use this command.")
-                        .color("#FF0000"));
-                return;
-            }
-        }
         ctx.sendMessage(this.getUsageString(ctx.sender()));
     }
 }
